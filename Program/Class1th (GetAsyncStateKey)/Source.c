15cf18703efd32e720a0042cc4ce8a6f921ae412
LeeMyeonggun <mk7396@naver.com>
2024-09-12 21:47:20 +0900

    (2024.9.12) 별이동 게임

--- Program/Class1th (GetAsyncStateKey)/Source.c
+++ Program/Class1th (GetAsyncStateKey)/Source.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <windows.h>
+#include <conio.h>
+#include <string.h>
 
 void Position(int x, int y)
 {
@@ -11,19 +13,128 @@ void Position(int x, int y)
 }
 
 
+#define UP 72
+#define LEFT 75
+#define RIGHT 77
+#define DOWN 80
+
+
+int screenIndex;
+HANDLE screen[2];
+
+void Initialize()
+{
+	CONSOLE_CURSOR_INFO cursor;
+
+	// 화면 버퍼를 2개 생성합니다.
+	screen[0] = CreateConsoleScreenBuffer
+	(
+		GENERIC_READ | GENERIC_WRITE,
+		0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL
+	);
+
+	screen[1] = CreateConsoleScreenBuffer
+	(
+		GENERIC_READ | GENERIC_WRITE,
+		0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL
+	);
+
+	cursor.dwSize = 1;
+	cursor.bVisible = FALSE;
+	SetConsoleCursorInfo(screen[0], &cursor);
+	SetConsoleCursorInfo(screen[1], &cursor);
+}
+
+void Flip()
+{
+	SetConsoleActiveScreenBuffer(screen[screenIndex]);
+
+	//   0              1
+	screenIndex = !screenIndex;
+
+
+}
+
+void Clear()
+{
+
+	COORD position = { 0, 0 };
+
+	DWORD dword;
+
+	FillConsoleOutputCharacter
+	(
+		screen[screenIndex], ' ', 50 * 20, position, &dword
+	);
+
+}
+
+void Release()
+{
+	CloseHandle(screen[0]);
+	CloseHandle(screen[1]);
+
+}
+
+void Render(int x, int y, const char* string)
+{
+	DWORD dword;
+	COORD position = { x, y };
+
+	SetConsoleCursorPosition(screen[screenIndex], position);
+	WriteFile(screen[screenIndex], string, strlen(string), &dword, NULL);
+
+}
+
 
 int main()
 {
+	char key = 0;
+
+	int x = 0;
+	int y = 0;
+	
+	Initialize();
+		
 
 	while (1)
 	{
-		Position(10, 10);
-		printf("☆");
 
-		Position(5, 5);
-		printf("★");
+		Flip();
 
-		system("cls");
+		Clear();
+
+		key = _getch();
+
+		if (key == -32)
+		{
+			key = _getch();
+		}
+				
+		
+		switch (key)
+		{
+		case UP:
+			y--;
+			break;
+
+		case LEFT:
+			x -= 2;
+			break;
+
+		case RIGHT:
+			x += 2;
+			break;
+
+		case DOWN:
+			y++;
+			break;
+	
+		}
+
+		Render(x, y, "☆");
+
+		
 	}
 	
 
